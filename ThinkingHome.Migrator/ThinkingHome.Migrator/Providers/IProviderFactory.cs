21ee2ddcd2079e34bf64d26e8b45795c34544cb6
Dmitry Andriyanov <dima117a@yandex-team.ru>
2016-11-15 23:41:00 +0300

    provider factory

---
+++ ThinkingHome.Migrator/ThinkingHome.Migrator/Providers/IProviderFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using ThinkingHome.Migrator.Framework.Interfaces;
+
+namespace ThinkingHome.Migrator.Providers
+{
+    public interface IProviderFactory
+    {
+        ITransformationProvider CreateProvider(string connectionString);
+        ITransformationProvider CreateProvider(IDbConnection connection);
+        IDbConnection CreateConnection(string connectionString);
+    }
+}
---
+++ ThinkingHome.Migrator/ThinkingHome.Migrator/Providers/ProviderFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using ThinkingHome.Migrator.Framework.Interfaces;
+
+namespace ThinkingHome.Migrator.Providers
+{
+    public abstract class ProviderFactory<TProvider, TConnection> : IProviderFactory
+        where TProvider : TransformationProvider<TConnection>
+        where TConnection : IDbConnection
+    {
+        protected abstract TProvider CreateProviderInternal(TConnection connection);
+        protected abstract TConnection CreateConnectionInternal(string connectionString);
+
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            return CreateConnectionInternal(connectionString);
+        }
+
+        public ITransformationProvider CreateProvider(IDbConnection connection)
+        {
+            if (!(connection is TConnection)) throw new InvalidCastException();
+
+            return CreateProviderInternal((TConnection) connection);
+        }
+
+        public ITransformationProvider CreateProvider(string connectionString)
+        {
+            var connection = CreateConnectionInternal(connectionString);
+
+            return CreateProviderInternal(connection);
+        }
+    }
+}
