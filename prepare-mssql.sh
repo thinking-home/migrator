cd8bc9b0d16a9b5ca74c73aaf21e3c19defa4e8e
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-04-28 01:07:09 +0300

    update .travis.yml

--- prepare-mssql.sh
+++ prepare-mssql.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo 12312313213213213213213213213213213213213213213
+
+echo $SA_PASSWORD
+
 /opt/mssql-tools/bin/sqlcmd -U sa -P "x987(!)654" -Q "SELECT @@VERSION"
 
 /opt/mssql-tools/bin/sqlcmd -U sa -P "x987(!)654" -Q "CREATE DATABASE migrations"
