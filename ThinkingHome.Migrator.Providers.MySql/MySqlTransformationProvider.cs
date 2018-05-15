4f4e31867b4cc172e35734372cdd3f06cf0395c1
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-05-15 10:49:49 +0300

    fix codestyle

--- ThinkingHome.Migrator.Providers.MySql/MySqlTransformationProvider.cs
+++ ThinkingHome.Migrator.Providers.MySql/MySqlTransformationProvider.cs
@@ -8,206 +8,209 @@ using ThinkingHome.Migrator.Framework.Extensions;
 
 namespace ThinkingHome.Migrator.Providers.MySql
 {
-	using ForeignKeyConstraint = Framework.ForeignKeyConstraint;
+    using ForeignKeyConstraint = Framework.ForeignKeyConstraint;
 
     public class MySqlTransformationProvider : TransformationProvider<MySqlConnection>
     {
-		public MySqlTransformationProvider(MySqlConnection connection, ILogger logger)
-			: base(connection, logger)
-		{
-			typeMap.Put(DbType.AnsiStringFixedLength, "CHAR(255)");
-			typeMap.Put(DbType.AnsiStringFixedLength, 255, "CHAR($l)");
-			typeMap.Put(DbType.AnsiStringFixedLength, 65535, "TEXT");
-			typeMap.Put(DbType.AnsiStringFixedLength, 16777215, "MEDIUMTEXT");
-			typeMap.Put(DbType.AnsiString, "VARCHAR(255)");
-			typeMap.Put(DbType.AnsiString, 255, "VARCHAR($l)");
-			typeMap.Put(DbType.AnsiString, 65535, "TEXT");
-			typeMap.Put(DbType.AnsiString, 16777215, "MEDIUMTEXT");
-			typeMap.Put(DbType.Binary, "LONGBLOB");
-			typeMap.Put(DbType.Binary, 127, "TINYBLOB");
-			typeMap.Put(DbType.Binary, 65535, "BLOB");
-			typeMap.Put(DbType.Binary, 16777215, "MEDIUMBLOB");
-			typeMap.Put(DbType.Boolean, "TINYINT(1)");
-			typeMap.Put(DbType.Byte, "TINYINT UNSIGNED");
-			typeMap.Put(DbType.Currency, "MONEY");
-			typeMap.Put(DbType.Date, "DATE");
-			typeMap.Put(DbType.DateTime, "DATETIME");
-			typeMap.Put(DbType.Decimal, "NUMERIC");
-			typeMap.Put(DbType.Decimal, 38, "NUMERIC($l, $s)", 2);
-			typeMap.Put(DbType.Double, "DOUBLE");
-			typeMap.Put(DbType.Guid, "VARCHAR(40)");
-			typeMap.Put(DbType.Int16, "SMALLINT");
-			typeMap.Put(DbType.Int32, "INTEGER");
-			typeMap.Put(DbType.Int64, "BIGINT");
-			typeMap.Put(DbType.Single, "FLOAT");
-			typeMap.Put(DbType.StringFixedLength, "CHAR(255)");
-			typeMap.Put(DbType.StringFixedLength, 255, "CHAR($l)");
-			typeMap.Put(DbType.StringFixedLength, 65535, "TEXT");
-			typeMap.Put(DbType.StringFixedLength, 16777215, "MEDIUMTEXT");
-			typeMap.Put(DbType.String, "VARCHAR(255)");
-			typeMap.Put(DbType.String, 255, "VARCHAR($l)");
-			typeMap.Put(DbType.String, 65535, "TEXT");
-			typeMap.Put(DbType.String, 16777215, "MEDIUMTEXT");
-			typeMap.Put(DbType.Time, "TIME");
+        public MySqlTransformationProvider(MySqlConnection connection, ILogger logger)
+            : base(connection, logger)
+        {
+            typeMap.Put(DbType.AnsiStringFixedLength, "CHAR(255)");
+            typeMap.Put(DbType.AnsiStringFixedLength, 255, "CHAR($l)");
+            typeMap.Put(DbType.AnsiStringFixedLength, 65535, "TEXT");
+            typeMap.Put(DbType.AnsiStringFixedLength, 16777215, "MEDIUMTEXT");
+            typeMap.Put(DbType.AnsiString, "VARCHAR(255)");
+            typeMap.Put(DbType.AnsiString, 255, "VARCHAR($l)");
+            typeMap.Put(DbType.AnsiString, 65535, "TEXT");
+            typeMap.Put(DbType.AnsiString, 16777215, "MEDIUMTEXT");
+            typeMap.Put(DbType.Binary, "LONGBLOB");
+            typeMap.Put(DbType.Binary, 127, "TINYBLOB");
+            typeMap.Put(DbType.Binary, 65535, "BLOB");
+            typeMap.Put(DbType.Binary, 16777215, "MEDIUMBLOB");
+            typeMap.Put(DbType.Boolean, "TINYINT(1)");
+            typeMap.Put(DbType.Byte, "TINYINT UNSIGNED");
+            typeMap.Put(DbType.Currency, "MONEY");
+            typeMap.Put(DbType.Date, "DATE");
+            typeMap.Put(DbType.DateTime, "DATETIME");
+            typeMap.Put(DbType.Decimal, "NUMERIC");
+            typeMap.Put(DbType.Decimal, 38, "NUMERIC($l, $s)", 2);
+            typeMap.Put(DbType.Double, "DOUBLE");
+            typeMap.Put(DbType.Guid, "VARCHAR(40)");
+            typeMap.Put(DbType.Int16, "SMALLINT");
+            typeMap.Put(DbType.Int32, "INTEGER");
+            typeMap.Put(DbType.Int64, "BIGINT");
+            typeMap.Put(DbType.Single, "FLOAT");
+            typeMap.Put(DbType.StringFixedLength, "CHAR(255)");
+            typeMap.Put(DbType.StringFixedLength, 255, "CHAR($l)");
+            typeMap.Put(DbType.StringFixedLength, 65535, "TEXT");
+            typeMap.Put(DbType.StringFixedLength, 16777215, "MEDIUMTEXT");
+            typeMap.Put(DbType.String, "VARCHAR(255)");
+            typeMap.Put(DbType.String, 255, "VARCHAR($l)");
+            typeMap.Put(DbType.String, 65535, "TEXT");
+            typeMap.Put(DbType.String, 16777215, "MEDIUMTEXT");
+            typeMap.Put(DbType.Time, "TIME");
 
-			propertyMap.RegisterPropertySql(ColumnProperty.Unsigned, "UNSIGNED");
-			propertyMap.RegisterPropertySql(ColumnProperty.Identity, "AUTO_INCREMENT");
+            propertyMap.RegisterPropertySql(ColumnProperty.Unsigned, "UNSIGNED");
+            propertyMap.RegisterPropertySql(ColumnProperty.Identity, "AUTO_INCREMENT");
+        }
 
-		}
+        #region custom sql
 
-		#region custom sql
+        public override bool IdentityNeedsType => false;
 
-		public override bool IdentityNeedsType => false;
+        protected override string NamesQuoteTemplate => "`{0}`";
 
-	    protected override string NamesQuoteTemplate => "`{0}`";
+        #endregion
 
-	    #endregion
+        #region custom sql
 
-		#region custom sql
+        protected override string GetSqlChangeColumnType(SchemaQualifiedObjectName table, string column,
+            ColumnType columnType)
+        {
+            string columnTypeSql = typeMap.Get(columnType);
 
-		protected override string GetSqlChangeColumnType(SchemaQualifiedObjectName table, string column, ColumnType columnType)
-		{
-			string columnTypeSql = typeMap.Get(columnType);
+            return FormatSql("ALTER TABLE {0:NAME} MODIFY {1:NAME} {2}", table, column, columnTypeSql);
+        }
 
-			return FormatSql("ALTER TABLE {0:NAME} MODIFY {1:NAME} {2}", table, column, columnTypeSql);
-		}
+        protected override string GetSqlDefaultValue(object defaultValue)
+        {
+            if (defaultValue is bool)
+            {
+                defaultValue = ((bool) defaultValue) ? 1 : 0;
+            }
 
-		protected override string GetSqlDefaultValue(object defaultValue)
-		{
-			if (defaultValue is bool)
-			{
-				defaultValue = ((bool)defaultValue) ? 1 : 0;
-			}
+            return $"DEFAULT {defaultValue}";
+        }
 
-			return $"DEFAULT {defaultValue}";
-		}
-
-		protected override string GetSqlRemoveConstraint(SchemaQualifiedObjectName table, string name)
-		{
-		    string constraintSql = (name ?? string.Empty).ToUpper() == "PRIMARY"
-		        ? "PRIMARY KEY"
+        protected override string GetSqlRemoveConstraint(SchemaQualifiedObjectName table, string name)
+        {
+            string constraintSql = (name ?? string.Empty).ToUpper() == "PRIMARY"
+                ? "PRIMARY KEY"
                 : FormatSql("KEY {0:NAME}", name);
 
             return FormatSql("ALTER TABLE {0:NAME} DROP {1}", table, constraintSql);
-		}
+        }
 
-		/// <summary>
-		/// MySql can migrate the table to another schema when renaming.
-		/// Therefore, you must explicitly add the schema of the source table to the new table name.
-		/// </summary>
-		protected override string GetSqlRenameTable(SchemaQualifiedObjectName oldName, string newName)
-		{
-			return FormatSql("ALTER TABLE {0:NAME} RENAME TO {1:NAME}", oldName, newName.WithSchema(oldName.Schema));
-		}
+        /// <summary>
+        /// MySql can migrate the table to another schema when renaming.
+        /// Therefore, you must explicitly add the schema of the source table to the new table name.
+        /// </summary>
+        protected override string GetSqlRenameTable(SchemaQualifiedObjectName oldName, string newName)
+        {
+            return FormatSql("ALTER TABLE {0:NAME} RENAME TO {1:NAME}", oldName, newName.WithSchema(oldName.Schema));
+        }
 
-		#endregion
+        #endregion
 
-		#region DDL
+        #region DDL
 
-		public override bool IndexExists(string indexName, SchemaQualifiedObjectName tableName)
-		{
-			string sql = FormatSql("SHOW INDEXES FROM {0:NAME}", tableName);
+        public override bool IndexExists(string indexName, SchemaQualifiedObjectName tableName)
+        {
+            string sql = FormatSql("SHOW INDEXES FROM {0:NAME}", tableName);
 
-			using (IDataReader reader = ExecuteReader(sql))
-			{
-				while (reader.Read())
-				{
-					if (reader["Key_name"].ToString() == indexName)
-					{
-						return true;
-					}
-				}
-			}
+            using (IDataReader reader = ExecuteReader(sql))
+            {
+                while (reader.Read())
+                {
+                    if (reader["Key_name"].ToString() == indexName)
+                    {
+                        return true;
+                    }
+                }
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		public override bool ConstraintExists(SchemaQualifiedObjectName table, string name)
-		{
-			string sqlConstraint = FormatSql("SHOW KEYS FROM {0:NAME}", table);
+        public override bool ConstraintExists(SchemaQualifiedObjectName table, string name)
+        {
+            string sqlConstraint = FormatSql("SHOW KEYS FROM {0:NAME}", table);
 
-			using (IDataReader reader = ExecuteReader(sqlConstraint))
-			{
-				while (reader.Read())
-				{
-					if (reader["Key_name"].ToString() == name)
-					{
-						return true;
-					}
-				}
-			}
+            using (IDataReader reader = ExecuteReader(sqlConstraint))
+            {
+                while (reader.Read())
+                {
+                    if (reader["Key_name"].ToString() == name)
+                    {
+                        return true;
+                    }
+                }
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		public override SchemaQualifiedObjectName[] GetTables(string schema = null)
-		{
-			string schemaSql = string.IsNullOrWhiteSpace(schema) ? "SCHEMA()" : $"'{schema}'";
+        public override SchemaQualifiedObjectName[] GetTables(string schema = null)
+        {
+            string schemaSql = string.IsNullOrWhiteSpace(schema) ? "SCHEMA()" : $"'{schema}'";
 
-			string sql = FormatSql(
-				"SELECT {0:NAME}, {1:NAME} FROM {2:NAME}.{3:NAME} WHERE {1:NAME} = {4}",
-				"TABLE_NAME", "TABLE_SCHEMA", "information_schema", "TABLES", schemaSql);
+            string sql = FormatSql(
+                "SELECT {0:NAME}, {1:NAME} FROM {2:NAME}.{3:NAME} WHERE {1:NAME} = {4}",
+                "TABLE_NAME", "TABLE_SCHEMA", "information_schema", "TABLES", schemaSql);
 
-			var tables = new List<SchemaQualifiedObjectName>();
+            var tables = new List<SchemaQualifiedObjectName>();
 
-			using (IDataReader reader = ExecuteReader(sql))
-			{
-				while (reader.Read())
-				{
-					string tableName = reader.GetString(0);
-					string tableSchema = reader.GetString(1);
-					tables.Add(tableName.WithSchema(tableSchema));
-				}
-			}
+            using (IDataReader reader = ExecuteReader(sql))
+            {
+                while (reader.Read())
+                {
+                    string tableName = reader.GetString(0);
+                    string tableSchema = reader.GetString(1);
+                    tables.Add(tableName.WithSchema(tableSchema));
+                }
+            }
 
-			return tables.ToArray();
-		}
+            return tables.ToArray();
+        }
 
-		public override bool TableExists(SchemaQualifiedObjectName table)
-		{
-			string sql = table.SchemaIsEmpty
-							? FormatSql("SHOW TABLES LIKE '{0}'", table)
-							: FormatSql("SHOW TABLES IN {0:NAME} LIKE '{1}'", table.Schema, table.Name);
+        public override bool TableExists(SchemaQualifiedObjectName table)
+        {
+            string sql = table.SchemaIsEmpty
+                ? FormatSql("SHOW TABLES LIKE '{0}'", table)
+                : FormatSql("SHOW TABLES IN {0:NAME} LIKE '{1}'", table.Schema, table.Name);
 
-			using (IDataReader reader = ExecuteReader(sql))
-			{
-				return reader.Read();
-			}
+            using (IDataReader reader = ExecuteReader(sql))
+            {
+                return reader.Read();
+            }
+        }
 
-		}
+        public override bool ColumnExists(SchemaQualifiedObjectName table, string column)
+        {
+            string sql = FormatSql("SHOW COLUMNS FROM {0:NAME} WHERE Field='{1}'", table, column);
 
-		public override bool ColumnExists(SchemaQualifiedObjectName table, string column)
-		{
-			string sql = FormatSql("SHOW COLUMNS FROM {0:NAME} WHERE Field='{1}'", table, column);
+            using (IDataReader reader = ExecuteReader(sql))
+            {
+                return reader.Read();
+            }
+        }
 
-			using (IDataReader reader = ExecuteReader(sql))
-			{
-				return reader.Read();
-			}
-		}
+        public override void RenameColumn(SchemaQualifiedObjectName tableName, string oldColumnName,
+            string newColumnName)
+        {
+            throw new NotSupportedException("MySql doesn't support column rename");
+        }
 
-		public override void RenameColumn(SchemaQualifiedObjectName tableName, string oldColumnName, string newColumnName)
-		{
-			throw new NotSupportedException("MySql doesn't support column rename");
-		}
+        public override void AddCheckConstraint(string name, SchemaQualifiedObjectName table, string checkSql)
+        {
+            throw new NotSupportedException("MySql doesn't support check constraints");
+        }
 
-		public override void AddCheckConstraint(string name, SchemaQualifiedObjectName table, string checkSql)
-		{
-			throw new NotSupportedException("MySql doesn't support check constraints");
-		}
+        public override void AddForeignKey(string name, SchemaQualifiedObjectName primaryTable, string[] primaryColumns,
+            SchemaQualifiedObjectName refTable, string[] refColumns,
+            ForeignKeyConstraint onDeleteConstraint = ForeignKeyConstraint.NoAction,
+            ForeignKeyConstraint onUpdateConstraint = ForeignKeyConstraint.NoAction)
+        {
+            if (onDeleteConstraint == ForeignKeyConstraint.SetDefault ||
+                onUpdateConstraint == ForeignKeyConstraint.SetDefault)
+            {
+                throw new NotSupportedException("MySQL foreign keys doesn't support SET DEFAULT action");
+            }
 
-		public override void AddForeignKey(string name, SchemaQualifiedObjectName primaryTable, string[] primaryColumns, SchemaQualifiedObjectName refTable, string[] refColumns, ForeignKeyConstraint onDeleteConstraint = ForeignKeyConstraint.NoAction, ForeignKeyConstraint onUpdateConstraint = ForeignKeyConstraint.NoAction)
-		{
-			if (onDeleteConstraint == ForeignKeyConstraint.SetDefault ||
-				onUpdateConstraint == ForeignKeyConstraint.SetDefault)
-			{
-				throw new NotSupportedException("MySQL foreign keys doesn't support SET DEFAULT action");
-			}
+            base.AddForeignKey(name, primaryTable, primaryColumns, refTable, refColumns, onDeleteConstraint,
+                onUpdateConstraint);
+        }
 
-			base.AddForeignKey(name, primaryTable, primaryColumns, refTable, refColumns, onDeleteConstraint, onUpdateConstraint);
-		}
-
-		#endregion
-
+        #endregion
     }
 }
--- ThinkingHome.Migrator.Tests/Providers/MySqlTransformationProviderTest.cs
+++ ThinkingHome.Migrator.Tests/Providers/MySqlTransformationProviderTest.cs
@@ -16,106 +16,106 @@ namespace ThinkingHome.Migrator.Tests.Providers
                 .CreateProvider("Database=migrations;Data Source=localhost;User Id=migrator;Password=123;", logger);
         }
 
-		#region Overrides of TransformationProviderTestBase<MySqlTransformationProvider>
+        #region Overrides of TransformationProviderTestBase<MySqlTransformationProvider>
 
-		protected override string GetSchemaForCompare()
-		{
-			return provider.ExecuteScalar("SELECT SCHEMA()").ToString();
-		}
+        protected override string GetSchemaForCompare()
+        {
+            return provider.ExecuteScalar("SELECT SCHEMA()").ToString();
+        }
 
-		protected override bool IgnoreCase => true;
+        protected override bool IgnoreCase => true;
 
-		protected override string BatchSql => @"
+        protected override string BatchSql => @"
 				insert into `BatchSqlTest` (`Id`, `TestId`) values (11, 111);
 				insert into `BatchSqlTest` (`Id`, `TestId`) values (22, 222);
 				insert into `BatchSqlTest` (`Id`, `TestId`) values (33, 333);";
 
-	    #endregion
+        #endregion
 
-		#region override tests
+        #region override tests
 
-		public override void CanVerifyThatCheckConstraintIsExist()
-		{
-			// todo: пройтись по всем тестам с NotSupportedException и проверить необходимость выдачи исключения
-			Assert.Throws<NotSupportedException>((Action) base.CanVerifyThatCheckConstraintIsExist);
-		}
+        public override void CanVerifyThatCheckConstraintIsExist()
+        {
+            // todo: пройтись по всем тестам с NotSupportedException и проверить необходимость выдачи исключения
+            Assert.Throws<NotSupportedException>((Action) base.CanVerifyThatCheckConstraintIsExist);
+        }
 
-		public override void CanAddCheckConstraint()
-		{
-			Assert.Throws<NotSupportedException>((Action) base.CanAddCheckConstraint);
-		}
+        public override void CanAddCheckConstraint()
+        {
+            Assert.Throws<NotSupportedException>((Action) base.CanAddCheckConstraint);
+        }
 
-		public override void CanRenameColumn()
-		{
-			Assert.Throws<NotSupportedException>((Action) base.CanRenameColumn);
-		}
+        public override void CanRenameColumn()
+        {
+            Assert.Throws<NotSupportedException>((Action) base.CanRenameColumn);
+        }
 
-		public override void CanAddForeignKeyWithDeleteSetDefault()
-		{
-			Assert.Throws<NotSupportedException>((Action) base.CanAddForeignKeyWithDeleteSetDefault);
-		}
+        public override void CanAddForeignKeyWithDeleteSetDefault()
+        {
+            Assert.Throws<NotSupportedException>((Action) base.CanAddForeignKeyWithDeleteSetDefault);
+        }
 
-		public override void CanAddForeignKeyWithUpdateSetDefault()
-		{
-			Assert.Throws<NotSupportedException>((Action) base.CanAddForeignKeyWithUpdateSetDefault);
-		}
+        public override void CanAddForeignKeyWithUpdateSetDefault()
+        {
+            Assert.Throws<NotSupportedException>((Action) base.CanAddForeignKeyWithUpdateSetDefault);
+        }
 
-	    public override void CanRollbackTransactions()
-	    {
-		    provider.AddTable("transtest3", new Column("id", DbType.Int32));
+        public override void CanRollbackTransactions()
+        {
+            provider.AddTable("transtest3", new Column("id", DbType.Int32));
 
-		    provider.BeginTransaction();
+            provider.BeginTransaction();
 
-		    provider.Insert("transtest3", new {id = 1});
-		    provider.Insert("transtest3", new {id = 2});
+            provider.Insert("transtest3", new {id = 1});
+            provider.Insert("transtest3", new {id = 2});
 
-		    Assert.Equal(2, (long) provider.ExecuteScalar("select count(*) from transtest3"));
+            Assert.Equal(2, (long) provider.ExecuteScalar("select count(*) from transtest3"));
 
-		    provider.Rollback();
+            provider.Rollback();
 
-		    Assert.Equal(0, (long) provider.ExecuteScalar("select count(*) from transtest3"));
+            Assert.Equal(0, (long) provider.ExecuteScalar("select count(*) from transtest3"));
 
-		    provider.TableExists("transtest3");
-	    }
+            provider.TableExists("transtest3");
+        }
 
-		#region primary key
+        #region primary key
 
-		// проверка, что в MySQL первичный ключ всегда называется "PRIMARY"
-	    [Fact]
-	    public void PrimaryKeyIsNamedPrimary()
-		{
-			// в отличие от стандартного теста, сравнение имен ключей происходит без учета регистра
-			string tableName = GetRandomName("TableWCPKmysql");
+        // проверка, что в MySQL первичный ключ всегда называется "PRIMARY"
+        [Fact]
+        public void PrimaryKeyIsNamedPrimary()
+        {
+            // в отличие от стандартного теста, сравнение имен ключей происходит без учета регистра
+            string tableName = GetRandomName("TableWCPKmysql");
 
-			provider.AddTable(tableName,
-				new Column("ID", DbType.Int32, ColumnProperty.PrimaryKey),
-				new Column("ID2", DbType.Int32, ColumnProperty.PrimaryKey)
-			);
+            provider.AddTable(tableName,
+                new Column("ID", DbType.Int32, ColumnProperty.PrimaryKey),
+                new Column("ID2", DbType.Int32, ColumnProperty.PrimaryKey)
+            );
 
-			Assert.True(provider.ConstraintExists(tableName, "PRIMARY"));
+            Assert.True(provider.ConstraintExists(tableName, "PRIMARY"));
 
-			provider.RemoveTable(tableName);
-		}
+            provider.RemoveTable(tableName);
+        }
 
-		public override void CanCheckThatPrimaryKeyIsExist()
-		{
-			string tableName = GetRandomName("CheckThatPrimaryKeyIsExist");
-			string pkName = GetRandomName("PK_CheckThatPrimaryKeyIsExist");
+        public override void CanCheckThatPrimaryKeyIsExist()
+        {
+            string tableName = GetRandomName("CheckThatPrimaryKeyIsExist");
+            string pkName = GetRandomName("PK_CheckThatPrimaryKeyIsExist");
 
-			provider.AddTable(tableName, new Column("ID", DbType.Int32, ColumnProperty.NotNull));
-			Assert.False(provider.ConstraintExists(tableName, pkName));
+            provider.AddTable(tableName, new Column("ID", DbType.Int32, ColumnProperty.NotNull));
+            Assert.False(provider.ConstraintExists(tableName, pkName));
 
-			provider.AddPrimaryKey(pkName, tableName, "ID");
-			Assert.True(provider.ConstraintExists(tableName, "PRIMARY"));
+            provider.AddPrimaryKey(pkName, tableName, "ID");
+            Assert.True(provider.ConstraintExists(tableName, "PRIMARY"));
 
-			provider.RemoveConstraint(tableName, "PRIMARY");
-			Assert.False(provider.ConstraintExists(tableName, "PRIMARY"));
+            provider.RemoveConstraint(tableName, "PRIMARY");
+            Assert.False(provider.ConstraintExists(tableName, "PRIMARY"));
 
-			provider.RemoveTable(tableName);
-		}
+            provider.RemoveTable(tableName);
+        }
 
-		#endregion
+        #endregion
 
-		#endregion
+        #endregion
     }
 }
