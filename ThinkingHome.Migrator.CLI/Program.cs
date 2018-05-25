d42a91ca72594c7b96e7ae5b13c5dc97881fd00d
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-05-25 20:26:14 +0300

    cli implementation

--- ThinkingHome.Migrator.CLI/Program.cs
+++ ThinkingHome.Migrator.CLI/Program.cs
@@ -1,25 +1,85 @@
 ﻿using System;
+using System.Reflection;
+using System.Text;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using ThinkingHome.Migrator.Framework.Extensions;
 
 namespace ThinkingHome.Migrator.CLI
 {
     public class Program
     {
-        private static int Invoke(string provider, string connectionString, string assembly,
+        private const long DEFAULT_VERSION = -1;
+
+        private static int Invoke(string provider, string connectionString, string assemblyPath,
             CommandOption versionOption, CommandOption timeoutOption, CommandOption listOption)
         {
+            using (var loggerFactory = new LoggerFactory())
+            {
+                loggerFactory.AddConsole();
 
+                var logger = loggerFactory.CreateLogger("migrate-database");
 
-            Console.WriteLine("Hello World!");
+                var asm = Assembly.LoadFile(assemblyPath);
+
+                using (var migrator = new Migrator(provider, connectionString, asm, logger))
+                {
+                    if (timeoutOption.HasValue())
+                    {
+                        migrator.Provider.CommandTimeout = Convert.ToInt32(timeoutOption.Value());
+                    }
+
+                    if (listOption.HasValue())
+                    {
+                        ShowList(migrator, logger);
+                    }
+                    else
+                    {
+                        Migrate(versionOption, migrator);
+                    }
+                }
+            }
+
             return 0;
+        }
+
+        private static void Migrate(CommandOption versionOption, Migrator migrator)
+        {
+            var targetVersion = versionOption.HasValue()
+                ? Convert.ToInt64(versionOption.Value())
+                : DEFAULT_VERSION;
+
+            migrator.Migrate(targetVersion);
+        }
+
+        private static void ShowList(Migrator migrator, ILogger logger)
+        {
+            var sb = new StringBuilder();
+            var appliedMigrations = migrator.GetAppliedMigrations();
+
+            sb.AppendLine("Available migrations:");
+            foreach (var info in migrator.AvailableMigrations)
+            {
+                var v = info.Version;
+                var marker = appliedMigrations.Contains(v) ? "✓" : " ";
+                var displayVersion = v.ToString().PadLeft(3);
+                var displayName = info.Type.Name.ToHumanName();
+
+                sb.AppendLine($"{marker} {displayVersion} {displayName}");
+            }
+
+            logger.LogInformation(sb.ToString());
         }
 
         static void Main(string[] args)
         {
+            var ver = Assembly.GetExecutingAssembly().GetName().Version;
+
             var app = new CommandLineApplication();
             app.Name = "migrate-database";
+            app.Description = $"ThinkingHome Database migrator - v{ver.Major}.{ver.Minor}.{ver.Revision}";
             app.HelpOption("-?|-h|--help");
-            app.Description = "Instruct the ninja to go and attack!";
+            app.ExtendedHelpText = "\nSee the details on https://github.com/thinking-home/migrator#readme.";
 
             var argProvider = app.Argument("provider", "Database provider alias or class name");
             var argConnectionString = app.Argument("connectionString", "Database connection string");
