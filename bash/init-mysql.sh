b9b4015aba3bafa328aeefafbc2a66aee8d435f5
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-06-17 13:06:57 +0300

    mysql strict mode

--- bash/init-mysql.sh
+++ bash/init-mysql.sh
@@ -12,8 +12,11 @@ mysql -e "CREATE USER 'migrator' IDENTIFIED BY '123';"
 echo "create alt user"
 mysql -e "CREATE USER 'migrator'@'localhost' IDENTIFIED BY '123';"
 
-echo "333"
+echo "grant privileges: user"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'migrator'@'%';"
 
-echo "444"
+echo "grant privileges: alt user"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'migrator'@'localhost';"
+
+echo "sql_mode: strict"
+mysql -e "SET GLOBAL sql_mode='STRICT_ALL_TABLES';"
