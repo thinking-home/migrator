5d53a1fb4ada9a5c0bd8cbd3247bad7961ff83a8
Dmitry Andriyanov <dima117a@yandex-team.ru>
2018-06-17 10:34:44 +0300

    fix mysql init script

--- bash/init-mysql.sh
+++ bash/init-mysql.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+echo "create main schema"
 mysql -e "CREATE SCHEMA migrations;"
 
+echo "create alt schema"
 mysql -e "CREATE SCHEMA Moo;"
 
+echo "create user"
 mysql -e "CREATE USER 'migrator' IDENTIFIED BY '123';"
 
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'migrator';"
+echo "create alt user"
+mysql -e "CREATE USER 'migrator'@'localhost' IDENTIFIED BY '123';"
+
+echo "333"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'migrator'@'%';"
+
+echo "444"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'migrator'@'localhost';"
