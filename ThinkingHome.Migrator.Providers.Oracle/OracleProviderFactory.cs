f652bc4d8611b8639505b27d7ac482f8765af8d2
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-05-01 01:55:31 +0300

    oracle provider

---
+++ ThinkingHome.Migrator.Providers.Oracle/OracleProviderFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
+using ThinkingHome.Migrator.Providers.Oracle;
+
+namespace ThinkingHome.Migrator.Providers.PostgreSQL
+{
+    public class OracleProviderFactory :
+        ProviderFactory<OracleTransformationProvider,OracleConnection>
+    {
+        protected override OracleTransformationProvider CreateProviderInternal(OracleConnection connection, ILogger logger)
+        {
+            return new OracleTransformationProvider(connection, logger);
+        }
+
+        protected override OracleConnection CreateConnectionInternal(string connectionString)
+        {
+            return new OracleConnection(connectionString);
+        }
+    }
+}
