f158e4b9be666ecb4d1e9a742402c776ccda9620
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-05-01 18:49:25 +0300

    oracle init script

--- bash/init-oracle.sh
+++ bash/init-oracle.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export PATH=/u01/app/oracle/product/11.2.0/xe/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export ORACLE_SID=XE
+
 sqlplus /nolog <<EOF
 connect SYS/oracle AS SYSDBA;
 
