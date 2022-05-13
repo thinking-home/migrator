16f70465669be24d7e20ef911d6981a01b419a44
dima117 <dima117a@yandex-team.ru>
2022-05-13 19:46:52 +0300

    Update init-postgres.sh

--- bash/init-postgres.sh
+++ bash/init-postgres.sh
@@ -2,8 +2,8 @@
 
 export PGPASSWORD=123
 
-psql -c "CREATE DATABASE migrations;" -U postgres --host=postgres
+psql -c "CREATE DATABASE migrations;" -U postgres
 
-psql -c "CREATE USER migrator WITH PASSWORD '123';" -U postgres --host=postgres
+psql -c "CREATE USER migrator WITH PASSWORD '123';" -U postgres
 
-psql -c 'CREATE SCHEMA "Moo" AUTHORIZATION migrator;' -U postgres -d migrations --host=postgres
+psql -c 'CREATE SCHEMA "Moo" AUTHORIZATION migrator;' -U postgres -d migrations
